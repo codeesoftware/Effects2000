25c32c94e4db29f4d6aa8126939021303005ff38
Páll Gábor <sorstenson@gmail.com>
2020-11-18 23:34:24 +0100

    w

---
+++ Effects2000/Utils/ParameterDelegateCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Effects2000.Utils
+{
+    public class ParameterDelegateCommand<TParameter> : ICommand
+    {
+        private readonly Func<bool> canExecute;
+        private readonly Action<TParameter> execute;
+        public ParameterDelegateCommand(Func<bool> canExecute, Action<TParameter> execute)
+        {
+            if (execute == null) throw new ArgumentNullException("execute", "Cannot be null!");
+
+            this.canExecute = canExecute;
+            this.execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute();
+        }
+
+        private void OnCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            execute((TParameter)parameter);
+        }
+    }
+}
--- Effects2000/ViewModels/MainViewModel.cs
+++ Effects2000/ViewModels/MainViewModel.cs
@@ -1,20 +1,38 @@
 ﻿using Effects2000.Utils;
-using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Media;
 
 namespace Effects2000.ViewModels
 {
-    class MainViewModel:BaseViewModel
+    class MainViewModel : BaseViewModel
     {
 
-        public DelegateCommand YesCommand { get; private set; }
+        public ParameterDelegateCommand<string> YesCommand { get; private set; }
 
-
+        Dictionary<string, string> sounds;
         protected override void Initlialize()
         {
             base.Initlialize();
 
+            //player.Load();
+            //player.Play();
+            sounds = new Dictionary<string, string> { { "yes",Directory.GetCurrentDirectory()+ @"\Assets\yes.wav" } };
+            YesCommand = new ParameterDelegateCommand<string>(() => true, Play);
+        }
+
+        //private void Yes()
+        //{
+        //    Play("yes");
+        //}
+
+        void Play(string soundName)
+        {
+            using (var player = new SoundPlayer(sounds[soundName.ToLower()]))
+            {
+                player.LoadAsync();
+                player.Play();
+            }
         }
     }
 }
